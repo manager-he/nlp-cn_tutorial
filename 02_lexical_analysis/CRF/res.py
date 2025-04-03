1c54a9098a7226b43bc00687c8fdfe638c377a6b
manager-He <13221769+manager-he@user.noreply.gitee.com>
2025-04-03 18:28:17 +0800

    over

--- 02_lexical_analysis/CRF/res.py
+++ 02_lexical_analysis/CRF/res.py
@@ -3,68 +3,65 @@ import torch
 
 class myCRF:
     def __init__(self):
-        self.scoreMap = {}  # 分数表
+        self.scoreMap = {}  # 特征分数表
         self.UnigramTemplates = []  # 状态特征模板
         self.BigramTemplates = []  # 转移特征模板
         self.readTemplate("template.txt")  # 读取特征模板
-        self.character_tagging("train_set.txt", "train_2.txt")  # 原始数据集加工
+        self.character_tagging("train_set.txt", "train_2.txt")  # 加工原始数据集
 
     def readTemplate(self, template_file, debug=False):
         '''
-        读取特征模板
+        读取特征模板文件并解析
         '''
         def parse_template_line(line):
-            """解析模板行的辅助函数"""
+            """解析模板行"""
             tmpList = []
-            if line.find("/") > 0:
-                left = line.split("/")[0].split("[")[-1].split(",")[0]
-                right = line.split("/")[-1].split("[")[-1].split(",")[0]
-                tmpList.append(int(left))
-                tmpList.append(int(right))
+            if "/" in line:
+                left, right = [int(x.split("[")[-1].split(",")[0]) for x in line.split("/")]
+                tmpList.extend([left, right])
             else:
-                num = line.split("[")[-1].split(",")[0]
-                tmpList.append(int(num))
+                tmpList.append(int(line.split("[")[-1].split(",")[0]))
             return tmpList
 
         with open(template_file, encoding='utf-8') as tempFile:
-            switchFlag = False  # 先读Unigram，再读Bigram
+            switchFlag = False  # 切换标志，先读Unigram，再读Bigram
             for line in tempFile:
-                if line.find("Unigram") > 0 or line.find("Bigram") > 0:  # 读到'Unigram'或者'Bigram'
+                line = line.strip()
+                if "Unigram" in line or "Bigram" in line:
                     continue
                 if switchFlag:
-                    if len(line.strip()) == 0:
-                        continue
-                    self.BigramTemplates.append(parse_template_line(line))
+                    if line:
+                        self.BigramTemplates.append(parse_template_line(line))
                 else:
-                    if len(line.strip()) == 0:
+                    if not line:
                         switchFlag = True
                     else:
                         self.UnigramTemplates.append(parse_template_line(line))
         if debug:
-            print(self.UnigramTemplates)
-            print(self.BigramTemplates)
+            print("Unigram Templates:", self.UnigramTemplates)
+            print("Bigram Templates:", self.BigramTemplates)
 
     def character_tagging(self, input_file, output_file):
         '''
-        将原始数据集加工成4-tag形式
+        将原始数据集加工成4-tag形式（B, M, E, S）
         '''
+        def write_tags(word, output_data):
+            """根据词长度写入对应标签"""
+            if len(word) == 1:
+                output_data.write(f"{word}\tS\n")
+            else:
+                output_data.write(f"{word[0]}\tB\n")
+                output_data.writelines(f"{char}\tM\n" for char in word[1:-1])
+                output_data.write(f"{word[-1]}\tE\n")
+
         with open(input_file, 'r', encoding='utf-8') as input_data, \
              open(output_file, 'w', encoding='utf-8') as output_data:
             for line in input_data:
-                word_list = line.strip().split(":")
-                word_list = word_list[-1].strip().split(" ")
-                for word in word_list[:]:
-                    words = word.split("/")
-                    if len(words) >= 2:
-                        xz = words[1]
-                        word = words[0]
-                        if len(word) == 1:
-                            output_data.write(word + "\tS\n")
-                        else:
-                            output_data.write(word[0] + "\tB\n")
-                            for w in word[1:-1]:
-                                output_data.write(w + "\tM\n")
-                            output_data.write(word[-1] + "\tE\n")
+                word_list = line.strip().split(":")[-1].strip().split(" ")
+                for word in word_list:
+                    parts = word.split("/")
+                    if len(parts) >= 2:
+                        write_tags(parts[0], output_data)
                 output_data.write("\n")
 
     def getTrainData(self, train_file):
@@ -94,28 +91,41 @@ class myCRF:
 
     def makeKey(self, template, identity, sentence, pos, statusCovered, debug=False):
         '''
-        模板标注函数：找出一句句子中，给定的模板下的某字符某标志相关的特征（BMES）
-        :param template: 给定特征模板
-        :param identity: 模板序号
-        :param sentence: 标注句子
-        :param pos: 当点位置
-        :param statusCovered: 状态标注
-        :param debug: 调试用
-        :return: 标注结果
+        模板标注函数：生成特征键
         '''
-        result = ""
-        result += identity
-        for i in template:
-            index = pos + i
-            if index < 0 or index >= len(sentence):
-                result += " "
-            else:
-                result += sentence[index]
-        result += "/"
-        result += statusCovered
-        if (debug == True):
+        result = identity + ''.join(
+            sentence[pos + i] if 0 <= pos + i < len(sentence) else " " for i in template
+        ) + f"/{statusCovered}"
+        if debug:
             print(result)
         return result
+
+    def updateScoreMap(self, key, weight_delta):
+        '''
+        更新特征分数表
+        '''
+        if key not in self.scoreMap:
+            self.scoreMap[key] = [1, weight_delta]
+        else:
+            self.scoreMap[key][1] += weight_delta
+
+    def updateFeatures(self, sentence, word, myResI, theoryResI, learning_rate):
+        '''
+        更新Unigram和Bigram特征
+        '''
+        for i, template in enumerate(self.UnigramTemplates):
+            myKey = self.makeKey(template, str(i), sentence, word, myResI)
+            theoryKey = self.makeKey(template, str(i), sentence, word, theoryResI)
+            self.updateScoreMap(myKey, -learning_rate)
+            self.updateScoreMap(theoryKey, learning_rate)
+
+        for i, template in enumerate(self.BigramTemplates):
+            prev_my = " " + myResI if word == 0 else myResI[word - 1:word + 1]
+            prev_theory = " " + theoryResI if word == 0 else theoryResI[word - 1:word + 1]
+            myKey = self.makeKey(template, str(i), sentence, word, prev_my)
+            theoryKey = self.makeKey(template, str(i), sentence, word, prev_theory)
+            self.updateScoreMap(myKey, -learning_rate)
+            self.updateScoreMap(theoryKey, learning_rate)
 
     def getUnigramScore(self, sentence, thisPos, thisStatus):
         '''
@@ -223,43 +233,9 @@ class myCRF:
         '''
         myRes = self.Viterbi(sentence)  # 我的解
         learning_rate = 0.1  # 学习率
-        for word in range(len(sentence)):  # 遍历整个句子
-            myResI = myRes[word]  # 我的解
-            theoryResI = realRes[word]  # 理论解
-            if myResI != theoryResI:  # 如果和理论值不同
-                # 更新Unigram特征
-                for i, template in enumerate(self.UnigramTemplates):
-                    myKey = self.makeKey(template, str(i), sentence, word, myResI)
-                    theoryKey = self.makeKey(template, str(i), sentence, word, theoryResI)
-                    # 更新我的解的权重
-                    if myKey not in self.scoreMap:
-                        self.scoreMap[myKey] = [1, -learning_rate]
-                    else:
-                        self.scoreMap[myKey][1] -= learning_rate
-                    # 更新理论解的权重
-                    if theoryKey not in self.scoreMap:
-                        self.scoreMap[theoryKey] = [1, learning_rate]
-                    else:
-                        self.scoreMap[theoryKey][1] += learning_rate
-
-                # 更新Bigram特征
-                for i, template in enumerate(self.BigramTemplates):
-                    if word == 0:
-                        myKey = self.makeKey(template, str(i), sentence, word, " " + myResI)
-                        theoryKey = self.makeKey(template, str(i), sentence, word, " " + theoryResI)
-                    else:
-                        myKey = self.makeKey(template, str(i), sentence, word, myRes[word - 1:word + 1])
-                        theoryKey = self.makeKey(template, str(i), sentence, word, realRes[word - 1:word + 1])
-                    # 更新我的解的权重
-                    if myKey not in self.scoreMap:
-                        self.scoreMap[myKey] = [1, -learning_rate]
-                    else:
-                        self.scoreMap[myKey][1] -= learning_rate
-                    # 更新理论解的权重
-                    if theoryKey not in self.scoreMap:
-                        self.scoreMap[theoryKey] = [1, learning_rate]
-                    else:
-                        self.scoreMap[theoryKey][1] += learning_rate
+        for word in range(len(sentence)):
+            if myRes[word] != realRes[word]:
+                self.updateFeatures(sentence, word, myRes[word], realRes[word], learning_rate)
 
     def Viterbi(self, sentence):
         '''
@@ -268,39 +244,33 @@ class myCRF:
         :return: 路径
         '''
         lens = len(sentence)
-        statusFrom = [[""] * lens, [""] * lens, [""] * lens, [""] * lens]  # B,M,E,S
-        maxScore = [[0] * lens, [0] * lens, [0] * lens, [0] * lens]  # 4条路
-        for word in range(0, lens):
-            for stateNum in range(0, 4):
+        maxScore = [[0] * lens for _ in range(4)]  # 4条路径
+        statusFrom = [[""] * lens for _ in range(4)]  # B, M, E, S
+
+        for word in range(lens):
+            for stateNum in range(4):
                 thisStatus = self.num2Tag(stateNum)
-                # 第一个词，状态特征加转移特征
                 if word == 0:
-                    uniScore = self.getUnigramScore(sentence, 0, thisStatus)
-                    biScore = self.getBigramScore(sentence, 0, " ", thisStatus)
-                    maxScore[stateNum][0] = uniScore + biScore
+                    maxScore[stateNum][0] = self.getUnigramScore(sentence, 0, thisStatus) + \
+                                            self.getBigramScore(sentence, 0, " ", thisStatus)
                     statusFrom[stateNum][0] = None
                 else:
-                    # 前面的所有路径到当前节点路径的所有得分之和
-                    scores = [0] * 4
-                    for i in range(0, 4):
-                        preStatus = self.num2Tag(i)  # 记录前一节点
-                        transScore = maxScore[i][word - 1]  # 到前一节点的路径和
-                        uniScore = self.getUnigramScore(sentence, word, thisStatus)  # 状态特征分数
-                        biScore = self.getBigramScore(sentence, word, preStatus, thisStatus)  # 转移特征分数
-                        scores[i] = transScore + uniScore + biScore  # 当前节点分数
-                    maxIndex = self.getMaxIndex(scores)  # 找到最大分数
-                    maxScore[stateNum][word] = scores[maxIndex]  # 最大分数记录
-                    statusFrom[stateNum][word] = self.num2Tag(maxIndex)  # 最大分数对应节点记录
+                    scores = [
+                        maxScore[i][word - 1] +
+                        self.getUnigramScore(sentence, word, thisStatus) +
+                        self.getBigramScore(sentence, word, self.num2Tag(i), thisStatus)
+                        for i in range(4)
+                    ]
+                    maxIndex = self.getMaxIndex(scores)
+                    maxScore[stateNum][word] = scores[maxIndex]
+                    statusFrom[stateNum][word] = self.num2Tag(maxIndex)
+
         resBuf = [""] * lens
-        scoreBuf = [0] * 4
         if lens > 0:
-            for i in range(0, 4):
-                scoreBuf[i] = maxScore[i][lens - 1]  # 最后一个字的各个标志最大分数
-            resBuf[lens - 1] = self.num2Tag(self.getMaxIndex(scoreBuf))  # 最后一个字最大分数对应标志
-        for backIndex in range(lens - 2, -1, -1):
-            resBuf[backIndex] = statusFrom[self.tag2Num(resBuf[backIndex + 1])][backIndex + 1]  # 回溯路径
-        res = "".join(resBuf)  # 输出路径
-        return res
+            resBuf[-1] = self.num2Tag(self.getMaxIndex([maxScore[i][-1] for i in range(4)]))
+            for backIndex in range(lens - 2, -1, -1):
+                resBuf[backIndex] = statusFrom[self.tag2Num(resBuf[backIndex + 1])][backIndex + 1]
+        return "".join(resBuf)
 
     def train_with_gradient_descent(self, data, model_path, epochnum=3, learning_rate=0.01):
         '''
@@ -326,35 +296,7 @@ class myCRF:
                     myResI = myRes[word]
                     theoryResI = result[word]
                     if myResI != theoryResI:
-                        # 更新Unigram特征
-                        for j, template in enumerate(self.UnigramTemplates):
-                            myKey = self.makeKey(template, str(j), sentence, word, myResI)
-                            theoryKey = self.makeKey(template, str(j), sentence, word, theoryResI)
-                            if myKey not in self.scoreMap:
-                                self.scoreMap[myKey] = [1, -learning_rate]
-                            else:
-                                self.scoreMap[myKey][1] -= learning_rate
-                            if theoryKey not in self.scoreMap:
-                                self.scoreMap[theoryKey] = [1, learning_rate]
-                            else:
-                                self.scoreMap[theoryKey][1] += learning_rate
-
-                        # 更新Bigram特征
-                        for j, template in enumerate(self.BigramTemplates):
-                            if word == 0:
-                                myKey = self.makeKey(template, str(j), sentence, word, " " + myResI)
-                                theoryKey = self.makeKey(template, str(j), sentence, word, " " + theoryResI)
-                            else:
-                                myKey = self.makeKey(template, str(j), sentence, word, myRes[word - 1:word + 1])
-                                theoryKey = self.makeKey(template, str(j), sentence, word, result[word - 1:word + 1])
-                            if myKey not in self.scoreMap:
-                                self.scoreMap[myKey] = [1, -learning_rate]
-                            else:
-                                self.scoreMap[myKey][1] -= learning_rate
-                            if theoryKey not in self.scoreMap:
-                                self.scoreMap[theoryKey] = [1, learning_rate]
-                            else:
-                                self.scoreMap[theoryKey][1] += learning_rate
+                        self.updateFeatures(sentence, word, myResI, theoryResI, learning_rate)
 
                 # 计算损失
                 wrongNum = self.getWrongNum(sentence, result)
@@ -432,58 +374,48 @@ class myCRF:
 
     def predict(self, sentence, parameter):
         '''
-        "解码"函数
-        :param sentence: 句子
-        :param parameter: 参数
-        :return:分词结果
+        解码函数：根据输入句子和模型参数进行分词预测
         '''
-        # global count
-        retsentence = []
-        state = []
-        # count += 1
-        # print(count)
         self.scoreMap = parameter['scoreMap']
         self.UnigramTemplates = parameter['UnigramTemplates']
         self.BigramTemplates = parameter['BigramTemplates']
-        retsentence = self.Viterbi(sentence)
-        if retsentence[-1] == 'B' or retsentence[-1] == 'M':  # 最后一个字状态不是'S'或'E'则修改
-            if retsentence[-2] == 'B'or retsentence[-2] == 'M':
-                retsentence[-1] = 'E'
-            else:
-                retsentence[-1] = 'S'
 
-        # 开始对该行分词
-        curLine = ''
-        # 遍历该行每一个字
-        for i in range(len(sentence)):
-            # 在列表中放入该字
-            curLine += sentence[i]
-            # 如果该字是S->单个词  或  E->结尾词 ，则在该字后面加上分隔符 |
-            # 此外如果改行的最后一个字了，也就不需要加 |
-            if (retsentence[i] == 'S'or retsentence[i] == 'E') and i != (len(sentence) - 1):
-                curLine += '|'
-        # 在返回列表中添加分词后的该行
-        state.append(curLine)
-        return state
+        # 使用Viterbi算法预测状态序列
+        predicted_states = self.Viterbi(sentence)
+
+        # 修正最后一个字的状态
+        if predicted_states[-1] in {'B', 'M'}:
+            predicted_states[-1] = 'E' if predicted_states[-2] in {'B', 'M'} else 'S'
+
+        # 根据状态序列进行分词
+        segmented_sentence = []
+        current_word = ''
+        for i, char in enumerate(sentence):
+            current_word += char
+            if predicted_states[i] in {'S', 'E'} and i != len(sentence) - 1:
+                current_word += '|'
+        segmented_sentence.append(current_word)
+        return segmented_sentence
 
 
 if __name__ == '__main__':
     model = myCRF()
-    # model.myTrain("train.txt", "CRF-dataSet copy.model", epochnum=7)
-    parameter = torch.load("CRF-dataSet copy.model")
-    # test_file_path = "test.txt"
-    # sentences = []
-    # with open(test_file_path, "r", encoding="utf-8") as f:
-    #     for line in f:
-    #         sentences.append(line.strip())
-    #         print(model.predict(line.strip(), parameter))
     
-    # s = "明明明明明白白白喜欢他可是他就是不说。"
-    # s = "人要是行，干一行行一行，一行行行行行，行行行干哪行都行。要是不行，干一行不行一行，一行不行行行不行，行行不行干哪行都不行。"
-    # s = "重庆市长江边的景色迷人，让人流连忘返。"
-    # s = "他说的确实在理，这里没有小明要的乒乓球拍，因为乒乓球拍卖完了。"
-    # s = "校长说：校服上除了校徽别别别的，让你们别别别的别别别的你非别别的！"
-    # s = "北京大学生在北京大学学习期间，积极参与研究生命起源的跨学科项目，致力于揭示生命演化的奥秘。"
-    ss = ["中文信息处理课程由同济大学卫老师讲授。", "中国的历史源远流长，有着上下五千年的历史。", "当代大学生在追求梦想的同时，也面临着现实生活中不断变化的竞争和压力。", "两千里的河堤，已经完全支离破碎了，许多地方被敌伪挖成了封锁沟，许多地方被农民改成了耕地。再加上风吹雨打，使许多段河堤连痕迹都没有了。", "最高人民法院认定该区块链金融合约因违反反洗钱法相关条款而无效。", "计算机科学与技术学院的研究生们在国际知名期刊上发表了大量高水平论文，获得广泛认可。"]
-    for s in ss:
-        print(model.predict(s, parameter))
+    # model.myTrain("train.txt", "CRF-dataSet final.model", epochnum=2)
+    
+    # 加载模型参数
+    parameter = torch.load("CRF-dataSet final.model")
+
+    # 示例句子
+    test_sentences = [
+        "中文信息处理课程由同济大学卫老师讲授。",
+        "中国的历史源远流长，有着上下五千年的历史。",
+        "当代大学生在追求梦想的同时，也面临着现实生活中不断变化的竞争和压力。",
+        "两千里的河堤，已经完全支离破碎了，许多地方被敌伪挖成了封锁沟，许多地方被农民改成了耕地。再加上风吹雨打，使许多段河堤连痕迹都没有了。",
+        "最高人民法院认定该区块链金融合约因违反反洗钱法相关条款而无效。",
+        "计算机科学与技术学院的研究生们在国际知名期刊上发表了大量高水平论文，获得广泛认可。"
+    ]
+
+    # 输出分词结果
+    for sentence in test_sentences:
+        print(model.predict(sentence, parameter))
